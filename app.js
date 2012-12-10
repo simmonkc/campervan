d6bad7ddf4833c39356bad991dbfa93ff2222d95
Kyle Simmons <simmonkc@gmail.com>
2012-12-11 00:08:27 +1300

    swapped out images for green blocks

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/images/:imageId', routes.show);
+// app.get('/images/:imageId', routes.show);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 exports.index = function(req, res) {
   images = [
-    { '_id' : 0, 'href' : '#0', 'title' : 'Tom cooking', 'lat' : '0.0', 'lng' : '0.2', 'createdDate' : new Date().getTime() },
-    { '_id' : 1, 'href' : '#1', 'title' : 'Kyle driving', 'lat' : '0.5', 'lng' : '3.2', 'createdDate' : new Date().getTime() },
-    { '_id' : 2, 'href' : '#2', 'title' : 'Arthur Pass', 'lat' : '1.0', 'lng' : '1.2', 'createdDate' : new Date().getTime() },
-    { '_id' : 3, 'href' : '#3', 'title' : 'Camper van shot', 'lat' : '2.0', 'lng' : '4.2', 'createdDate' : new Date().getTime() }
+    { '_id' : 123451, 'href' : '#0', 'title' : 'Tom cooking', 'lat' : '0.0', 'lng' : '0.2', 'createdDate' : new Date().getTime() },
+    { '_id' : 123452, 'href' : '#1', 'title' : 'Kyle driving', 'lat' : '0.5', 'lng' : '3.2', 'createdDate' : new Date().getTime() },
+    { '_id' : 123453, 'href' : '#2', 'title' : 'Arthur Pass', 'lat' : '1.0', 'lng' : '1.2', 'createdDate' : new Date().getTime() },
+    { '_id' : 123454, 'href' : '#3', 'title' : 'Camper van shot', 'lat' : '2.0', 'lng' : '4.2', 'createdDate' : new Date().getTime() },
+    { '_id' : 123455, 'href' : '#3', 'title' : 'Camper van shot', 'lat' : '2.0', 'lng' : '4.2', 'createdDate' : new Date().getTime() }
   ];
   res.render('index', { title: 'Campervan', images : images });
 };
