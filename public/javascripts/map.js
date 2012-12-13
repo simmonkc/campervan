1f6406682b2f521ba15be6fdbcfa65558e77c4b1
Kyle Simmons <simmonkc@gmail.com>
2012-12-13 22:06:42 +1300

    google maps centering

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,35 +1,52 @@
 /* Code to grab custom map for contact section */	
-	
-	var map;
-	var infoWindow;
-	var auckland = new google.maps.LatLng(-36.847739,174.764231);
-	var mapCenter = new google.maps.LatLng(-39.282443,174.762331);
-	
-
       function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-39.282443,174.762331),
+          zoom: 5,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+        var marker = new google.maps.Marker({
+		      position: new google.maps.LatLng(-36.847739,174.764231),
+		      map: map,
+		      title: "Auckland, New Zealand"		  
+		      });
+		    google.maps.event.addListener(marker,'click', function(){
+				
+				  console.log("Lorem Ipsum");
+						
+		    });
+      }
+      
+
+
+      /*
+function initialize() {
 
         var mapOptions = {
-          zoom: 6,
-          center: mapCenter,
+          zoom: 3,
+          center: ,
           maxZoom: 12,
-          minZoom: 5,
+          minZoom: 3,
           mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+            mapTypeIds: [google.maps.MapTypeId.SATELLITE]
           }
         };
                
-        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+        var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
         
-        var marker = new google.maps.Marker({
-		      position: auckland,
+*/
+       /*
+ var marker = new google.maps.Marker({
+		      position: new google.maps.LatLng(-36.847739,174.764231),
 		      map: map,
 		      title: "Auckland, New Zealand"		  
 		});
-		
-		
-		/*
-google.maps.event.addListener(marker,'click', function(){
-						
-		});
 */
-      }
+		
+				/*
+
+
+		
+      };*/ 
