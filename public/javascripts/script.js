03659b7b7d6771d6f88163536b5f7e69b46f582b
Kyle Simmons <simmonkc@gmail.com>
2012-12-14 17:53:42 +1300

    updated list styling and overlay styles

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     
+
+    
     var loadSelectedImage = function(id){    
       var imageURL = 'http://localhost:3000/images/' + id + '.jpg';
+      $('.overlay').show();
+      $('.selected-image img').attr("src", imageURL);
         
     };
     
     $('.image-list li a').click(function(){
         loadSelectedImage($(this).attr("id"));
+        
     });
     
     $('.close').click(function(e){
