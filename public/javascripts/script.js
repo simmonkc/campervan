c56c98cedc31f1ad4a6ba5b45216c20a42d96d92
Tom Buchok <git@buchok.com>
2012-12-14 18:20:12 +1300

    use event dispatching for opening the overlays

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
     
+    $('body').on('loadSelectedImage', function(event, href){
+      $('.overlay').show();
+      $('.selected-image img').attr("src", href);
+      $('body').css("overflow-y","hidden");
+    });
 
-    
     var loadSelectedImage = function(href){    
       $('.overlay').show();
       $('.selected-image img').attr("src", href);
       $('body').css("overflow-y","hidden");
-        
     };
     
     $('.image-list li a').click(function(e){
-        loadSelectedImage($(this).attr("href"));
         e.preventDefault();        
+        $('body').trigger('loadSelectedImage', $(this).attr('href'));
     });
     
     $('.close').click(function(e){
