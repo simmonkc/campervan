39830019cc0050d17dc0d80d2bbca822cf50ce44
Kyle Simmons <simmonkc@gmail.com>
2012-12-20 15:05:30 +1300

    updated the css and script files for the overlay to significantly improve usability

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     
     $('body').on('loadSelectedImage', function(event, href){
+	    var windowHeight = $(window).height();
+
       $('.overlay').show();
-      $('.selected-image img').attr("src", href);
+      $('.selected-image img').attr("src", href).css("max-height", windowHeight);
       $('body').css("overflow-y","hidden");
     });
 
     var loadSelectedImage = function(href){    
+    	    
       $('.overlay').show();
       $('.selected-image img').attr("src", href);
       $('body').css("overflow-y","hidden");
